f7bbf91cd38e51a88155e4b72b4dd0c77bd14b31
Ekow Abaka Ainooson <jainooson@gmail.com>
2023-01-16 14:12:03 -0600

    Getting the photo a day plugin going

--- photo_a_day/_foonoo/plugins/photo_a_day/photo_a_day/PhotoADayPlugin.php
+++ photo_a_day/_foonoo/plugins/photo_a_day/photo_a_day/PhotoADayPlugin.php
@@ -4,6 +4,7 @@ namespace foonoo\plugins\photo_a_day\photo_a_day;
 
 use foonoo\Plugin;
 use foonoo\events\PluginsInitialized;
+use foonoo\events\SiteWriteStarted;
 use foonoo\sites\SiteFactoryInterface;
 use foonoo\sites\AbstractSite;
 
@@ -13,9 +14,8 @@ class PhotoADayPlugin extends Plugin implements SiteFactoryInterface
     public function getEvents() 
     {
         return [
-            PluginsInitialized::class => function(PluginsInitialized $event) {
-                $event->getSiteTypeRegistry()->register($this, "photo-a-day");
-            }
+            PluginsInitialized::class => fn (PluginsInitialized $event) => $event->getSiteTypeRegistry()->register($this, "photo-a-day"),
+            SiteWriteStarted::class => fn (SiteWriteStarted $event) => $event->getThemeManager()->prependToThemePath(__DIR__ . "/themes")
         ];
     }
 
