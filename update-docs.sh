405a1c6cf9592ce35b2b056de6cca5ba724632d0
Ekow Abaka Ainooson <jainooson@gmail.com>
2023-08-04 11:14:46 -0500

    Working on making the shared items symbolic links

--- update-docs.sh
+++ update-docs.sh
@@ -2,5 +2,12 @@
 
 # Clearice
 git clone https://github.com/ekowabaka/clearice /tmp/clearice
-cp -r /tmp/clearice/docs clearice/docs
+echo "Copying clearice content to docs"
+cp -rv /tmp/clearice/docs/* clearice/docs
+
+# Foonoo Documentation
+git clone https://github.com/foonoo/docs /tmp/foonoo-docs
+echo "Copying fooboo content to docs"
+cp -rv /tmp/foonoo-docs/* foonoo/docs
+
 
