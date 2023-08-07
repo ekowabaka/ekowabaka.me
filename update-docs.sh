1749447d9de26f3a30b12c6329cee302f6b311c6
Ekow Abaka Ainooson <jainooson@gmail.com>
2023-08-06 20:16:20 -0500

    Removing documentation files

--- update-docs.sh
+++ update-docs.sh
@@ -3,11 +3,11 @@
 # Clearice
 git clone https://github.com/ekowabaka/clearice /tmp/clearice
 echo "Copying clearice content to docs"
-cp -rv /tmp/clearice/docs/* clearice/docs
+cp -rv /tmp/clearice/docs clearice/docs
 
 # Foonoo Documentation
 git clone https://github.com/foonoo/docs /tmp/foonoo-docs
 echo "Copying fooboo content to docs"
-cp -rv /tmp/foonoo-docs/* foonoo/docs
+cp -rv /tmp/foonoo-docs foonoo/docs
 
 
