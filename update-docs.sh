2f97b41ac3e8af9cd4985c2da521e1a74c3ab1bc
Ekow Abaka Ainooson <jainooson@gmail.com>
2023-08-21 00:40:07 -0500

    A new update docs script

--- update-docs.sh
+++ update-docs.sh
@@ -4,10 +4,13 @@
 git clone https://github.com/ekowabaka/clearice /tmp/clearice
 echo "Copying clearice content to docs"
 cp -rv /tmp/clearice/docs clearice/docs
+chmod a-w -R clearice/docs/*
+chmod u+w -R clearice/docs/_foonoo
 
 # Foonoo Documentation
 git clone https://github.com/foonoo/docs /tmp/foonoo-docs
 echo "Copying fooboo content to docs"
 cp -rv /tmp/foonoo-docs foonoo/docs
-
+chmod a-w -R foonoo/docs/*
+chmod u+w -R foonoo/docs/_foonoo
 
